b83f1c0412cfeafc1208cf924763b0b952c82741
umara4 <90219191+umara4@users.noreply.github.com>
2024-01-20 12:06:33 -0700

    power of two encoding for kmp algorithm
    and power of two script generation

---
+++ create_twos.py
@@ -0,0 +1,68 @@
+import random
+from itertools import chain, combinations
+import sys
+
+
+def gen_twos(alphabet_size):
+    twos = []
+    power = 0
+    while power < alphabet_size:
+        twos.append(2** power)
+        power += 1
+    return twos
+
+def create_indet_letter(alphabet):
+    indet_letter = 0
+    subset_size = random.randint(2, len(alphabet) - 1)
+    subset = random.sample(range(len(alphabet)), subset_size)
+    for i in subset:
+        indet_letter += alphabet[i] 
+    return indet_letter
+
+def gen_solid_string(length, alphabet_size):
+    text = []
+    alphabet = gen_twos(alphabet_size)
+    for i in range(length):
+        text.append(alphabet[random.randint(0, alphabet_size - 1)])
+    return text
+
+def gen_indet_string(text, alphabet_size, num):
+    alphabet = gen_twos(alphabet_size)
+    indet_list = random.sample(range(len(text)), num)
+    for i in indet_list:
+        text[i] = create_indet_letter(alphabet)
+    return text
+
+def generate_text_pattern(
+    text_length, text_indet_letters, pattern_length, pattern_indet_letters, alphabet
+):
+    text = gen_solid_string(text_length, alphabet)
+    text = gen_indet_string(text, alphabet, text_indet_letters)
+    pattern = gen_solid_string(pattern_length, alphabet)
+    pattern = gen_indet_string(pattern, alphabet, pattern_indet_letters)
+    f = open(
+        "demofile_patterns.txt", "w"
+    )
+    for i in pattern:
+        f.write(str(i) + ",")
+    f.close()
+
+    f2 = open("demofile_texts.txt", "w")
+    for i in text:
+        f2.write(str(i) + ",")
+    f2.close()
+
+
+args = sys.argv[1:]
+if len(args) != 5:
+    print("Usage: python3 script_name text_length k1 pattern_length k2 sigma")
+    sys.exit(1)
+
+val1 = int(args[0])
+val2 = int(args[1])
+val3 = int(args[2])
+val4 = int(args[3])
+val5 = int(args[4])
+
+
+generate_text_pattern(val1, val2, val3, val4, val5)
